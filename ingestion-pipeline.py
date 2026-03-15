ca58c8b2f717907e43334b064a0091bf795c4ec4
surya-moorthy <tssuryamoorthy20092005@gmail.com>
2026-03-15 15:10:56 +0530

    completed coding for splitting docs into chunks and storing it in database

--- ingestion-pipeline.py
+++ ingestion-pipeline.py
@@ -35,10 +35,60 @@ def load_documents(docs_path="docs"):
 
     return documents
 
+def split_documents(documents, chunk_size=800, chunk_overlap=0):
+    """Split documents into smaller chunks with overlap"""
+    print("Splitting documents into chunks")
+
+    text_splitter = CharacterTextSplitter(
+        chunk_size=chunk_size,
+        chunk_overlap=chunk_overlap
+    )
+
+    chunks = text_splitter.split_documents(documents)
+
+    if chunks:
+        for i, chunk in enumerate(chunks[:5]):
+            print(f"------Chunk {i + 1}--------")
+            print(f"Source: {chunk.metadata['source']}")
+            print(f"Length: {len(chunk.page_content)} characters")
+            print(f"Content:")
+            print(chunk.page_content)
+            print("-"*50)
+        if len(chunks) > 5:
+            print(f"\n------- and {len(chunks) - 5} more chunks.....")
+    
+    return chunks
+
+def create_vector_store(chunks, persist_directory="db/chroma_db"):
+    """Create and persist ChromaDB vector store"""
+    print("Creating embeddings  and storing in ChromaDB...")
+
+    embedding_model = OllamaEmbeddings(model="nomic-embed-text:latest")
+
+    print("---Creating vector store---")
+    vector_store = Chroma.from_documents(
+        documents=chunks,
+        embedding=embedding_model,
+        persist_directory=persist_directory,
+        collection_metadata={"hnsw:space": "cosine"}
+    )
+
+    print("--- Finished creating vector store ---")
+
+    print(f"Vector store created and saved to {persist_directory}")
+    return vector_store
+
 def main():
     print("Main functions")
 
+    # loading documents
     documents = load_documents(docs_path="docs")
+
+    # chunking documents , which splits the docs into chunks
+    chunks = split_documents(documents, chunk_size=800, chunk_overlap=0)
+
+    # creating vector embeddings and stored it in a folder 
+    vector_store = create_vector_store(chunks=chunks, persist_directory="db/chroma_db")
 
 
 if __name__ == "__main__":
